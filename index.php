e9a3c90f944b8b5ba7779bf91b29774039819069
lemon8de <jayandrew.minon@gmail.com>
2024-07-19 12:52:03 +0800

    fixed syntax, added some checks when the full login if statement fails

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <!-- require the login.php api to do the login process -->
- <?php require 'process/login.php'
-
-
+ <?php require 'process/login.php';
     // confirming login session
     if (isset($_SESSION['username'])) {
-
         // choosing what redirect page the user should see
-        if (isset($_SESSION['role'] == 'admin')) {
-            header('location: page/admin/dashboard.php')
+        if (($_SESSION['role'] == 'ADMIN')) {
+            header('location: page/admin/dashboard.php');
             exit;
-        }elseif ($_SESSION['role'] == 'user') {
-            header('location: page/user/dashboard.php')
+        }elseif ($_SESSION['role'] == 'USER') {
+            header('location: page/user/dashboard.php');
             exit;
         }
+    } else {
+        echo 'php elsed on the loging screen';
+        echo 'possible issues is the login if statements are all falsed';
     }
  ?>
-"Hello World" 
+
+<!DOCTYPE html>
+<lang="en">
+    <head>
+        <meta charset = "utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Exercise 1</title>
+    </head>
+    <body>
+
+    </body>
+
+</html>
