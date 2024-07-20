a1bceac812b788f57361c3e59157af8bb940bf6c
lemon8de <jayandrew.minon@gmail.com>
2024-07-20 11:29:10 +0800

    ux changes

--- page/user/plugins/sidebar/user_bar.php
+++ page/user/plugins/sidebar/user_bar.php
@@ -2,7 +2,7 @@
   <!-- Brand Logo -->
   <a href="dashboard.php" class="brand-link">
     <img src="../../dist/img/logo.ico" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">Exercise1 | User</span>
+    <span class="brand-text font-weight-light">Portal | User</span>
   </a>
 
   <!-- Sidebar -->
@@ -26,7 +26,7 @@
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
-                <a href="dashboard.php" class="nav-link"><i class="nav-icon fas fa-address-book"></i><p>Details</p></a>
+                <a href="dashboard.php" class="nav-link"><i class="nav-icon fas fa-address-book"></i><p>Dashboard</p></a>
             </li>
 
             <li class="nav-item">
