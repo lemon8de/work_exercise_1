71e1dcbc04d4735cc52e05535c01c4db28f243d2
lemon8de <jayandrew.minon@gmail.com>
2024-07-20 13:25:04 +0800

    new api + files

--- page/admin/plugins/sidebar/admin_bar.php
+++ page/admin/plugins/sidebar/admin_bar.php
@@ -17,7 +17,7 @@
             </div>
             <div class="col-sm-9">
                 <div class="info">
-                    <a href="dashboard.php" class="d-block"><?=htmlspecialchars($_SESSION['name']) . '</br>' . $_SESSION['section'] . ' | ' . $_SESSION['position'];?></a>
+                <a href="dashboard.php" class="d-block"><?=htmlspecialchars($_SESSION['name']) . '</br>' . $_SESSION['id_number'] . '</br>' . $_SESSION['section'] . ' | ' . $_SESSION['position'];?></a>
                 </div>
             </div>
         </div>
@@ -26,15 +26,30 @@
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
-                <a href="accounts.php" class="nav-link"><i class="nav-icon fas fa-user-cog"></i><p>Account Management</p></a>
+                <a href="dashboard.php" class="nav-link"><i class="nav-icon fas fa-address-book"></i><p>Dashboard</p></a>
             </li>
 
-            <li class="nav-item">
-                <a href="dashboard.php" class="nav-link"><i class="nav-icon fas fa-bus"></i><p>Dashboard</p></a>
+            <li class="nav-item menu-is-opening menu-open">
+                <a href="#" class="nav-link"><i class="nav-icon far fa-envelope"></i><p>Alerts<i class="fas fa-angle-left right"></i></p></a>
+                <ul class="nav nav-treeview" style="display: block;">
+                    <li class="nav-item">
+                        <a href="alert/view.html" class="nav-link"><i class="fas fa-book nav-icon"></i><p>View</p></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="mailbox/create.html" class="nav-link"><i class="fas fa-plus nav-icon"></i><p>Create</p></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="mailbox/dismiss.html" class="nav-link"><i class="fas fa-times nav-icon"></i><p>Dismiss</p></a>
+                    </li>
+                </ul>
             </li>
 
             <li class="nav-item">
                 <a href="sample1.php" class="nav-link"><i class="nav-icon fas fa-user-cog"></i><p>Sample 1</p></a>
+            </li>
+
+            <li class="nav-item">
+                <a href="accounts.php" class="nav-link"><i class="nav-icon fas fa-user-cog"></i><p>Account Management</p></a>
             </li>
 
             <li class="nav-item">
